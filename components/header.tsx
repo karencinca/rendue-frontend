3f65fbb231360ba4a610f7e8afd3b20f65ecd5bc
Karen Cinca <karencinca1@gmail.com>
2024-01-10 00:28:15 -0300

    initial header

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,9 @@ import styles from './header.module.css'
 export default function Header() {
     return (
         <header className={styles.header}>
-            <p>HEADER</p>
+            <input type="text" placeholder='Pesquise por imóveis ou inquilinos...' />
+
+            <img src="https://jagsystem.com/img/sites/img_mulher2_ia.jpg" alt="imagem do usuário" />
         </header>
     )
 }
