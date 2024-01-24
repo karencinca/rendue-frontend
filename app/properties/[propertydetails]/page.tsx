34e842aca689ce27f38a6f87dcaefaed6382cfd9
Karen Cinca <karencinca1@gmail.com>
2024-01-24 00:30:47 -0300

    properties routes and styling adjustments

--- app/properties/[propertydetails]/page.tsx
+++ app/properties/[propertydetails]/page.tsx
@@ -1,7 +1,11 @@
-export default function PropertyDetails() {
+import styles from './page.module.css'
+
+export default function PropertyDetails({ params }) {
     return (
-        <>
+        <div className={styles.page}>
             <h1>Detalhes do imóvel</h1>
-        </>
+            <h2>Casa {params.propertydetails}</h2>
+
+        </div>
     )
 }
--- components/property-card.tsx
+++ components/property-card.tsx
@@ -1,5 +1,6 @@
 import { Properties } from '@/api'
 import styles from './property-card.module.css'
+import Link from 'next/link'
 
 interface PropertyCardProps{
     data: Array<Properties>
@@ -9,11 +10,13 @@ export default function PropertyCard({ data }: PropertyCardProps) {
     return (
         <div className={styles.cards}>
             {data.map(property => (
-                <li key={property.id}>
-                    <img src={`/${property.image}`} alt=""/>
-                    <h2>{property.title}</h2>
-                    <h3>{property.description}</h3>
-                </li>
+                <Link href={`properties/${property.id}`}>
+                    <li key={property.id}>
+                        <img src={`/${property.image}`} alt=""/>
+                        <h2>{property.title}</h2>
+                        <h3>{property.description}</h3>
+                    </li>
+                </Link>
             ))}
         </div>
     )
