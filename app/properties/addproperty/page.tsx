67f66212bb0155f8dfbcb205f88888133ca57760
Karen Cinca <karencinca1@gmail.com>
2024-01-24 21:20:25 -0300

    route add property page

--- app/properties/addproperty/page.tsx
+++ app/properties/addproperty/page.tsx
@@ -1,7 +1,24 @@
+import styles from './page.module.css'
+
 export default function AddProperty() {
     return (
-        <>
+        <div className={styles.container}>
             <h1>Adicionar imóvel</h1>
-        </>
+            <form>
+                <label htmlFor="property-name">Nome do imóvel:</label>
+                <input type="text" id="property-name" />
+
+                <label htmlFor="property-description">Descrição:</label>
+                <textarea name="property-description" id="property-description" cols="30" rows="3"></textarea>
+
+                <label htmlFor="property-address">Endereço:</label>
+                <input type="text" id="property-address" />
+
+                <label htmlFor="property-image">Foto do imóvel:</label>
+                <input type="file" />
+
+                <button type="submit">Adicionar</button>
+            </form>
+        </div>
     )
 }
--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -2,13 +2,18 @@ import styles from './page.module.css'
 import propertiesList from '../../api'
 import Button from '@/components/button'
 import PropertyCard from '@/components/property-card'
+import Link from 'next/link'
 
 export default function Properties() {
     return (
         <div className={styles.container}>
             <div>
                 <h1>Imóveis</h1>
-                <Button>Adicionar imóvel</Button>
+                <Link href="/properties/addproperty">
+                    <Button>
+                        Adicionar imóvel
+                    </Button>
+                </Link>
             </div>
             <ul>
                 <PropertyCard 
