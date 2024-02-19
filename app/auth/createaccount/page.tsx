2a8b3eb962a70169e77510b6c9495414bbdc4a69
Karen Cinca <karencinca1@gmail.com>
2024-02-18 21:28:31 -0300

    login page style

--- app/auth/createaccount/page.tsx
+++ app/auth/createaccount/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import styles from './styles.module.css'
+import styles from './page.module.css'
 
 export default function CreateAccount() {
     return (
--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,30 @@
+import Link from 'next/link'
+import styles from './page.module.css'
+
 export default function Login() {
     return (
-        <h1>Login</h1>
+            <div className={styles.container}>
+                <div className={styles.form_container}>
+                    <h1>Acessar conta</h1>
+                    <form>
+                        <div className={styles.wrapper}>
+                            <label htmlFor="email">Email:</label>
+                            <input type="email" id="email" />
+                        </div>
+
+                        <div className={styles.wrapper}>
+                            <label htmlFor="password">Senha:</label>
+                            <input type="password" id="password" />
+                        </div>
+
+                        <button type='submit'>Entrar</button>
+                    </form>
+                    <button>
+                        <Link href='/auth/createaccount'>
+                        Ainda não tenho uma conta
+                        </Link>
+                    </button>
+                </div>
+            </div>
     )
 }
